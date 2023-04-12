979147c138355df606a9bd3e0eaea8020e308ac9
C.Seng <74439890+cheeseig9@users.noreply.github.com>
2023-04-12 22:16:51 +0800

    Update build_docker_image.sh

--- build_docker_image.sh
+++ build_docker_image.sh
@@ -13,20 +13,20 @@ sudo systemctl enable docker
 # create a dockerfile
 
 # build the docker image
-sudo docker build -t techmax .
+sudo docker build -t it-web-sample-image .
 
 # login to your docker hub account
 # must create my_password.txt before use it
 cat ~/my_password.txt | sudo docker login --username chetseig9 --password-stdin
 
 # use the docker tag command to give the image a new name
-sudo docker tag techmax chetseig9/techseng-terraform
+sudo docker tag techmax chetseig9/it-web-sample-terraform
 
 # push the image to your docker hub repository
-sudo docker push chetseig9/techseng-terraform
+sudo docker push chetseig9/it-web-sample-terraform
 
 # start the container to test the image 
-sudo docker run -dp 80:80 chetseig9/techseng-terraform
+sudo docker run -dp 80:80 chetseig9/it-web-sample-terraform
 
 # referances
 # https://docs.aws.amazon.com/AmazonECS/latest/developerguide/create-container-image.html
